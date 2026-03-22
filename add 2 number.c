58237a22eae8e7a41fef6d710fed80c74a3baa59
shubhneetkaur08 <shubhneetk78@gmail.com>
2026-03-23 00:35:53 +0530

    Add files via upload

---
+++ add 2 number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a , b, sum;
+	printf("the value of a:");
+	scanf("%d",&a);
+	
+	printf("the value of b:");
+	scanf("%d",&b);
+	
+	sum= a+b;
+	printf(" sum = %d", sum);
+	 
+    return 0;
+}
+
+
+
